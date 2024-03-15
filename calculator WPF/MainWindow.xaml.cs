b9bdc447f709754e3bcf13b7732314d625fd4aea
123 <123@DESKTOP-1BLLIE5>
2024-03-16 08:26:49 +0900

    Добавьте файлы проекта.

---
+++ calculator WPF/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace calculator_WPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            string buttonValue = button.Content.ToString();
+
+            if (resultTextBlock.Text == "0")
+            {
+                resultTextBlock.Text = buttonValue;
+            }
+            else
+            {
+                resultTextBlock.Text += buttonValue;
+            }
+        }
+
+        private void Equals_Click(object sender, RoutedEventArgs e)
+        {
+            string expression = resultTextBlock.Text;
+            try
+            {
+                var result = new System.Data.DataTable().Compute(expression, null);
+                resultTextBlock.Text = result.ToString();
+            }
+            catch (Exception ex)
+            {
+                resultTextBlock.Text = "Ошибка";
+            }
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            resultTextBlock.Text = "0";
+        }
+    }
+}
